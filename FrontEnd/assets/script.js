5420bcc2030287a9567c557cb6631c6d78021d76
AmelieHennequez <hennequez.amelie@gmail.com>
2024-04-29 12:20:38 +0200

    debut

---
+++ FrontEnd/assets/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    async function logWorks() {
+        const reponse = await fetch("http://localhost:5678/api/works");
+        const works = await reponse.json();
+        const gallerie = document.getElementsByClassName("gallery");
+        let html = "";
+
+        works.forEach((item)=>{
+            const title = item["title"];
+            const image = item["imageUrl"];
+            let figure =`<figure>
+                <img src="${image}" alt="${title}">
+                <figcaption>${title}</figcaption>
+            </figure>`;
+            html += figure;
+       });
+
+gallerie.innerHTML = html;
+        
+
+
+    }
+
+    logWorks();
+});
